5a685b756d4f233f42c7e1178b63ed079425af18
Austin Hyde <austin109@gmail.com>
2021-12-01 23:25:51 -0500

    day 1 part 2

--- day1-sonar/src/main.rs
+++ day1-sonar/src/main.rs
@@ -2,13 +2,32 @@
 fn main() {
     println!("Part 1 answer: {}", part1(include_str!("part1.txt")));
     // answer: 1342
+
+    println!("Part 2 answer: {}", part2(include_str!("part1.txt")));
+    // answer: 1378
 }
 
 fn part1(input: &str) -> usize {
-    input.lines()
+    count_increases(
+        input.lines()
         .filter_map(|l| l.parse().ok())
-        .collect::<Vec<usize>>()
-        .windows(2)
+        .collect()
+    )
+}
+
+fn part2(input: &str) -> usize {
+    count_increases(
+        input.lines()
+            .filter_map(|l| l.parse().ok())
+            .collect::<Vec<usize>>()
+            .windows(3)
+            .map(|w| w.iter().sum())
+            .collect()
+    )
+}
+
+fn count_increases(v: Vec<usize>) -> usize {
+    v.windows(2)
         .filter(|w| w[1] > w[0])
         .count()
 }
@@ -21,5 +40,11 @@ mod tests {
     fn example1() {
         let input = include_str!("example1.txt");
         assert_eq!(part1(input), 7)
+    }
+
+    #[test]
+    fn example2() {
+        let input = include_str!("example1.txt");
+        assert_eq!(part2(input), 5);
     }
 }
